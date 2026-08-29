d8272b9b76bb0e02e1d9e0d4212f6dfe1863a4cb
nsase <128037624+nsase@users.noreply.github.com>
2026-08-29 16:21:41 +0000

    feat: add alternate OLL 11 algorithm

--- src/app/core/algorithm-cases/oll/11.ts
+++ src/app/core/algorithm-cases/oll/11.ts
@@ -9,5 +9,9 @@ export const OLL_11_CASE = defineOllCase({
   algorithms: [
     { id: 'f5273459-e0f0-47aa-ac04-395458222bee', notation: "Rw U R' U R' F R F' R U2 Rw'" },
     { id: '38f484df-d783-4016-a8e1-71deb5381110', notation: "(y2) Rw' R2 U R' U R U2 R' U M'" },
+    {
+      id: '15828a59-92f3-4bd7-a829-780e60363dbc',
+      notation: "(y') F' L' U' L U F U F R U R' U' F'",
+    },
   ],
 });
